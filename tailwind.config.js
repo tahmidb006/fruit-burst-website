ee509eb29e6002d9745552bac075a9e11d3323e5
Md Thamid <tahmidb006@gmail.com>
2023-07-27 23:28:58 +0600

    completed

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    purge: [],
+    darkMode: false, // Set to 'media' or 'class' if you want dark mode support
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [require('daisyui')],
+};
